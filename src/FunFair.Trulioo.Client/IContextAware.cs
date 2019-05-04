b8d8c34604007c57845ea084b44a47098c95512f
Amel Salibasic <nukecx@gmail.com>
2019-05-04 19:10:18 +0100

    chore: hello goodbye

--- src/FunFair.Trulioo.Client/IContextAware.cs
+++ src/FunFair.Trulioo.Client/IContextAware.cs
@@ -1,12 +1,11 @@
 ﻿namespace FunFair.Trulioo.Client
 {
     /// <summary>
-    /// 
     /// </summary>
     public interface IContextAware
     {
         /// <summary>
-        /// Gets the <see cref="Context"/> instance for this <see cref="ITruliooApiClient"/>.
+        ///     Gets the <see cref="Context" /> instance for this <see cref="ITruliooApiClient" />.
         /// </summary>
         Context Context { get; }
     }
