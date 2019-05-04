a030cec84d9e262109dd3b0f14254b5b53266679
Amel Salibasic <nukecx@gmail.com>
2019-05-04 18:20:22 +0100

    chore: run build

--- src/FunFair.Trulioo.Client/BusinessSearch.cs
+++ src/FunFair.Trulioo.Client/BusinessSearch.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using FunFair.Trulioo.Client.Model;
 using FunFair.Trulioo.Client.Model.BusinessSearch;
 using FunFair.Trulioo.Client.URI;
 
@@ -12,13 +10,13 @@ namespace FunFair.Trulioo.Client
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BusinessSearch"/> class.
+        ///     Initializes a new instance of the <see cref="BusinessSearch" /> class.
         /// </summary>
         /// <param name="service">
-        /// An object representing the root of Trulioo configuration service.
+        ///     An object representing the root of Trulioo configuration service.
         /// </param>
         /// <exception cref="ArgumentNullException">
-        /// <paramref name="service"/> is <c>null</c>.
+        ///     <paramref name="service" /> is <c>null</c>.
         /// </exception>
         protected internal BusinessSearch(TruliooApiClient service)
         {
@@ -33,26 +31,28 @@ namespace FunFair.Trulioo.Client
         #region Methods
 
         /// <summary>
-        /// Business Search call for Trulioo API Client V1
+        ///     Business Search call for Trulioo API Client V1
         /// </summary>
         /// <param name="request"> Request object containing parameters to search for </param>
         /// <returns> Contains the List of possible businesses from search </returns>
         public async Task<BusinessSearchResponse> BusinessSearchAsync(BusinessSearchRequest request)
         {
             var resource = new ResourceName("search");
-            var response = await _context.PostAsync<BusinessSearchResponse>(_businessNamespace, resource, request).ConfigureAwait(false);
+            var response = await _context.PostAsync<BusinessSearchResponse>(_businessNamespace, resource, request)
+                .ConfigureAwait(false);
             return response;
         }
 
         /// <summary>
-        /// Gets Business Search transaction information
+        ///     Gets Business Search transaction information
         /// </summary>
         /// <param name="transactionId"> TransactionRecordID of Business Search to retreive </param>
         /// <returns> Contains the Business Search transaction result </returns>
         public async Task<BusinessSearchResponse> BusinessSearchResultAsync(string id)
         {
-            var resource = new ResourceName("search", "transactionrecord",id);
-            var response = await _context.GetAsync<BusinessSearchResponse>(_businessNamespace, resource).ConfigureAwait(false);
+            var resource = new ResourceName("search", "transactionrecord", id);
+            var response = await _context.GetAsync<BusinessSearchResponse>(_businessNamespace, resource)
+                .ConfigureAwait(false);
             return response;
         }
 
@@ -62,7 +62,7 @@ namespace FunFair.Trulioo.Client
 
         private readonly TruliooApiClient _service;
 
-        private Context _context { get { return _service?.Context; } }
+        private Context _context => _service?.Context;
 
         private readonly Namespace _businessNamespace = new Namespace("business");
 
