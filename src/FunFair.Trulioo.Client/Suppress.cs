ecc820581ca9558bde2f8b5a62817e63d200eb87
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-04-20 23:49:41 +0100

    Suppressed the error as it's third party code

--- src/FunFair.Trulioo.Client/Suppress.cs
+++ src/FunFair.Trulioo.Client/Suppress.cs
@@ -1,3 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage(category: "Microsoft.Design", checkId: "CA1016:MarkAssembliesWithAssemblyVersion")]
+
+[assembly: SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0012", Justification = "Third party code")]
